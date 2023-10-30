df4cf3f6746a04f8f4c029a78342d6f0425225bc
alvaro <alvarocortesmartin@gmail.com>
2023-10-30 13:09:38 +0100

    js files adapted to node, holamundo and fibonacci

--- holamundo.js
+++ holamundo.js
@@ -1,2 +1,7 @@
-let nombre = 'alvaro'
-console.log('hola mundo'+ nombre);
+console.log("Programa para imprimir la clase");
+const name = "ASIR"
+let number = 5;
+
+for (let i = 0; i < number; i++) {
+	console.log(name)
+}
